ef7a909a69bd501cc6df6bbfee318e504f5f9d38
coderpwh <plmoknpwh@163.com>
2025-07-16 09:15:03 +0800

    Update src/component/list.rs
    Co-authored-by: stefan-mysten <135084671+stefan-mysten@users.noreply.github.com>

--- src/component/list.rs
+++ src/component/list.rs
@@ -10,7 +10,7 @@ pub async fn list_components() -> Result<()> {
     let components = crate::handlers::available_components();
     let mut table = Table::new();
     table.load_preset(TABLE_FORMAT)
-        .set_header(vec![Cell::new("Available Components")])
+        .set_header(vec![Cell::new("Available Binaries to Install")])
         .add_rows(
             components.iter().map(|component| {
                 vec![Cell::new(component)]
